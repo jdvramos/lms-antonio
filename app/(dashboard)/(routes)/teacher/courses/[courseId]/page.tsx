148cdf0c4df4e1494715df4d56fa3231fb745880
jdvramos <jonndenzelramos@gmail.com>
2023-11-02 12:41:03 +0800

    10_course_title_form_done

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,8 +3,9 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { LayoutDashboard } from "lucide-react";
-
 import { IconBadge } from "@/components/icon-badge";
+
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth();
@@ -52,6 +53,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                         <IconBadge icon={LayoutDashboard} />
                         <h2 className="text-xl">Customize your course</h2>
                     </div>
+                    <TitleForm initialData={course} courseId={course.id} />
                 </div>
             </div>
         </div>
