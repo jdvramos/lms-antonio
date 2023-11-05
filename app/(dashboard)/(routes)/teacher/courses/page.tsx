9787ca5b892e474eaf70d57fc1cca6412485f2dc
jdvramos <jonndenzelramos@gmail.com>
2023-11-05 12:08:52 +0800

    25_courses_data_table_done

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,12 +1,33 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+
+const CoursesPage = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    /*
+        Find all the courses created by the   
+        user logged in and sort by most recent
+    */
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
     return (
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>New Course</Button>
-            </Link>
+            <DataTable columns={columns} data={courses} />
         </div>
     );
 };
