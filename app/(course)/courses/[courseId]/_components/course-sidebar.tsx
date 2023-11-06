cbf7653849329e95a3e6301c64d92d77ffee6254
jdvramos <jonndenzelramos@gmail.com>
2023-11-06 09:27:27 +0800

    29_course_and_chapters_layout_done

---
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -0,0 +1,61 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { Chapter, Course, UserProgress } from "@prisma/client";
+
+import { CourseSidebarItem } from "./course-sidebar-item";
+
+interface CourseSidebarProps {
+    course: Course & {
+        chapters: (Chapter & {
+            userProgress: UserProgress[] | null;
+        })[];
+    };
+    progressCount: number;
+}
+
+export const CourseSidebar = async ({
+    course,
+    progressCount,
+}: CourseSidebarProps) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    /*
+        In our schema.prisma we created a composite unique constraint
+        @@unique([userId, courseId]), you can use userId_courseId to
+        query the result (compound identifier or composite key)
+    */
+    const purchase = await db.purchase.findUnique({
+        where: {
+            userId_courseId: {
+                userId,
+                courseId: course.id,
+            },
+        },
+    });
+
+    return (
+        <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
+            <div className="p-8 flex flex-col border-b">
+                <h1 className="font-semibold">{course.title}</h1>
+                {/* Check purchase and add progress */}
+            </div>
+            <div className="flex flex-col w-full">
+                {course.chapters.map((chapter) => (
+                    <CourseSidebarItem
+                        key={chapter.id}
+                        id={chapter.id}
+                        label={chapter.title}
+                        isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+                        courseId={course.id}
+                        isLocked={!chapter.isFree && !purchase}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
